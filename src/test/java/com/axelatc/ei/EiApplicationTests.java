62dc39896c71896a66658328335f674875993bf7
axelatc <axel.krebs@promsocatc.net>
2022-09-23 16:12:32 +0200

    Init commit
    Add template retrieved from spring Initializr at https://start.spring.io/
    Use the following URL to obtain the same ZIP archive I bootstraped:
    https://start.spring.io/#!type=maven-project&language=java
    &platformVersion=2.7.3&packaging=jar&jvmVersion=17&groupId=com
    .axelatc&artifactId=ei&name=ei&description=Projet d'Epreuve Intégrée
    2021-2022 aux Aumôniers du Travail&packageName=com.axelatc
    .ei&dependencies=lombok,web,data-rest,thymeleaf,security,oauth2-client,
    oauth2-resource-server,data-jpa,flyway,jooq,h2,mysql,actuator,restdocs

---
+++ src/test/java/com/axelatc/ei/EiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axelatc.ei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
